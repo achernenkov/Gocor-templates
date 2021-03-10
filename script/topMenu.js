ac72ebec8b09292c127a9605e33ac20da63bdfbf
achernenkov <jardagorng@gmail.com>
2021-03-10 18:09:26 +0300

    Completed menu.

---
+++ script/topMenu.js
@@ -0,0 +1,20 @@
+const burger = document.querySelector('.burger_menu')
+const first = document.querySelector('.first')
+const second = document.querySelector('.second')
+const third = document.querySelector('.third')
+const fourth = document.querySelector('.fourth')
+const topMenu = document.querySelector('.top_menu')
+const topMenuWrapper = document.querySelector('.top_menu_wrapper')
+const topMenuList = document.querySelector('.top_menu_list')
+
+burger.addEventListener('click', (ev) => {
+    ev.preventDefault()
+    first.classList.toggle('active')
+    second.classList.toggle('active')
+    third.classList.toggle('active')
+    fourth.classList.toggle('active')
+    topMenu.classList.toggle('active')
+    topMenuWrapper.classList.toggle('active')
+    topMenuList.classList.toggle('active')
+
+})
